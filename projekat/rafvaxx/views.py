0065779993a6a94b58773a32081cf812ae85ea96
relja-mikanovic <rmikanovic6719rn@raf.rs>
2021-09-15 16:04:53 +0200

    -Dodatak

--- projekat/rafvaxx/views.py
+++ projekat/rafvaxx/views.py
@@ -5,18 +5,19 @@ from .forms import *
 def dash(request):
     student = Student.objects.all()
     vaccine = Vaccine.objects.all()
-    user = User.objects.all()
     return render(request, 'rafvaxx/dashboard.html', {'student': student, 'vaccine': vaccine})
 
 def student(request):
     student = Student.objects.all()
     total_student = student.count()
-    procenat = total_student / 400 * 100
+    procenat = total_student / 50 * 100
     return render(request, 'rafvaxx/student.html', {'student': student, 'total_student': total_student, 'procenat': procenat})
 
 def vaccine(request):
     vaccine = Vaccine.objects.all()
-    return render(request, 'rafvaxx/vaccine.html', {'vaccine': vaccine})
+    student = Student.objects.all()
+    increment = 0
+    return render(request, 'rafvaxx/vaccine.html', {'vaccine': vaccine, 'student':student, 'increment':increment})
 
 def user(request):
     user = User.objects.all()
